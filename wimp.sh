1eb131aff2bc7467d84e3397a43e116c8e17fa2e
Anders Norman <anders.norman@barco.com>
2014-01-20 12:45:44 +0100

    Finding WiMP executable using pidof and /proc/PID/exe

--- wimp.sh
+++ wimp.sh
@@ -2,15 +2,21 @@
 
 COMMAND=$1
 
+WiMP=$(readlink -e /proc/$(pidof WiMP)/exe)
+if [ -z WiMP ]; then
+    echo "WiMP not found" 1>&2
+    exit 1
+fi
+
 case $COMMAND in
     playpause)
         xdotool search --onlyvisible wimp key space
         ;;
     prev)
-        /opt/WiMP/bin/WiMP previous
+        $WiMP previous
         ;;
     next)
-        /opt/WiMP/bin/WiMP next
+        $WiMP next
         ;;
     *)
         echo "Usage: $0 <command>" 1>&2
